d2aeaa671312980a831c91a1a6c02edffcca3409
herbert.fuchs <hmcfuchs@gmail.com>
2022-04-26 15:24:50 -0300

    DetailsScreen e navegação.

--- src/Screens/HomeScreen/index.tsx
+++ src/Screens/HomeScreen/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-export default function Home() {
+export default function Home({ navigation }) {
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Home</Text>
+            <Text>Home</Text>
+            <Button 
+                title="Go to Details"
+                onPress={() => navigation.navigate('Details')}
+            />
         </View>
     );
 }
--- src/routes/Stacks/MainStack.tsx
+++ src/routes/Stacks/MainStack.tsx
@@ -2,14 +2,24 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../../Screens/HomeScreen';
+import DetailsScreen from '../../Screens/DetailsScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes() {
   return (
     <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
+        <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen 
+              name="Home" 
+              component={Home}
+              options={{ title: 'Overview' }} 
+            />
+            <Stack.Screen 
+              name="Details" 
+              component={DetailsScreen} 
+              options={{ title: 'Details' }}
+            />
         </Stack.Navigator>
     </NavigationContainer>
   );
