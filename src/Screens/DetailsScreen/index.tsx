1049016460a26fdd91419e2910e8faacf875e89c
herbert.fuchs <hmcfuchs@gmail.com>
2022-04-26 15:27:26 -0300

    Diferenças entre navigate e push

--- src/Screens/DetailsScreen/index.tsx
+++ src/Screens/DetailsScreen/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-export default function DetailsScreen() {
+export default function DetailsScreen({ navigation }) {
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Details Screen</Text>
+            <Button 
+                title="Ir para a Details novamente..."
+                onPress={() => {navigation.push('Details')}}
+            />
         </View>
     );
 }
