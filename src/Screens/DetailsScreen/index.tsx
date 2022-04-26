2202083afd2c9dbbdf39de2ec35e5a60dad0bde4
herbert.fuchs <hmcfuchs@gmail.com>
2022-04-26 15:53:11 -0300

    Retornos, popToTop, navigate e goBack.

--- src/Screens/DetailsScreen/index.tsx
+++ src/Screens/DetailsScreen/index.tsx
@@ -5,9 +5,20 @@ export default function DetailsScreen({ navigation }) {
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Details Screen</Text>
+            <Text>Navigation.PUSH</Text>
             <Button 
                 title="Ir para a Details novamente..."
                 onPress={() => {navigation.push('Details')}}
+            />
+            <Text>Navigation.navigate para a HOME</Text>
+            <Button 
+                title="Ir para a Home"
+                onPress={() => {navigation.navigate('Home')}}
+            />
+            <Text>Navigation.popToTop, retorna todas as telas.</Text>
+            <Button 
+                title="Retornar"
+                onPress={() => {navigation.popToTop()}}
             />
         </View>
     );
